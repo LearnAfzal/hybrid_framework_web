c3aacdd691c9c71ca31fed7b1d96f461a34f0e15
Azalkhan <afzalkhan.inayathkhan@einfochips.com>
2023-03-26 17:15:49 +0530

    git commit -m "added invalid request demo method"

--- tests/test_requestdemo.py
+++ tests/test_requestdemo.py
@@ -37,6 +37,7 @@ class TestDemo(mainclass):
             self.driver.execute_script("window.scrollTo(0, 500)")
         if confirm_button.is_displayed():
             confirm_button.click()"""
+        time.sleep(3)
         self.driver.find_element(By.ID,"schedule-dropdownmenubtn").click()
         self.driver.find_element(By.XPATH,"//span[@class='cursor-pointer']").click()
         self.driver.find_element(By.XPATH,"//span[contains(text(),'Confirm Time')]").click()
@@ -47,3 +48,39 @@ class TestDemo(mainclass):
         print(success_msg)
         assert_that(success_msg).is_equal_to(SuccessMsg)
         time.sleep(5)
+
+    @pytest.mark.parametrize("FirstName,LastName,JobTitle,CompanyName,Email,Mobile,TotalEmp,Country,HearAbout,DemoName,InvalidField,ErrorMsg",data_source.test_invalid_request_demo)
+    def test_invalid_request_demo(self, FirstName, LastName, JobTitle, CompanyName, Email, Mobile, TotalEmp, Country,HearAbout, DemoName, InvalidField, ErrorMsg):
+            self.driver.find_element(By.ID, "gway-nav-demo").click()
+            self.driver.find_element(By.ID, "schedule_modal-firstname").send_keys(FirstName)
+            self.driver.find_element(By.ID, "schedule_modal-lastname").send_keys(LastName)
+            self.driver.find_element(By.ID, "schedule_modal-jobtitle").send_keys(JobTitle)
+            self.driver.find_element(By.ID, "schedule_modal-companyname").send_keys(CompanyName)
+            self.driver.find_element(By.ID, "schedule_modal-email").send_keys(Email)
+            self.driver.find_element(By.ID, "schedule_modal-phonenumber").send_keys(Mobile)
+            self.driver.find_element(By.ID, "schedule_modal-number").send_keys(TotalEmp)
+            self.driver.find_element(By.XPATH, "//div[@id='schedule_modal-country-wrapper']").click()
+            self.driver.find_element(By.XPATH, f"//span[@data-countrycode='{Country}']").click()
+            self.driver.find_element(By.ID, "schedule_modal-hearabout").send_keys(HearAbout)
+            cookies_msg = self.driver.find_element(By.XPATH, "//a[text()='I Accept']")
+            if cookies_msg.is_displayed():
+                cookies_msg.click()
+            self.driver.find_element(By.XPATH, "//a[@id='schedule_modal-product_DropdownMenuBtn']").click()
+            self.driver.find_element(By.XPATH, f"//span[contains(text(),'{DemoName}')]").click()
+            self.driver.find_element(By.XPATH, "//span[contains(text(),'Confirm Products')]").click()
+            """wait = WebDriverWait(self.driver, 30)
+            wait.until(expected_conditions.visibility_of_element_located((By.ID, "schedule-dropdownmenubtn")))"""
+            time.sleep(3)
+            self.driver.find_element(By.ID, "schedule-dropdownmenubtn").click()
+            self.driver.find_element(By.XPATH, "//span[@class='cursor-pointer']").click()
+            self.driver.find_element(By.XPATH, "//span[contains(text(),'Confirm Time')]").click()
+            self.driver.find_element(By.XPATH, "//span[text()='Submit']").click()
+            if(InvalidField=="Work Email"):
+                error_msg = self.driver.find_element(By.XPATH, "//span[text()='Please input a valid email address.']").text
+                print(error_msg)
+                assert_that(error_msg).is_equal_to(ErrorMsg)
+            elif(InvalidField=="Total Employees"):
+                error_msg = self.driver.find_element(By.XPATH,"//span[text()='Please input a valid number.']").text
+                print(error_msg)
+                assert_that(error_msg).is_equal_to(ErrorMsg)
+            time.sleep(5)
--- utilities/data_source.py
+++ utilities/data_source.py
@@ -6,3 +6,6 @@ print(test_invalid_login_data)
 test_valid_request_demo=read_utils.get_sheet_as_list("../test_data/RequestDemoData.xlsx","Valid_Data")
 print(test_valid_request_demo)
 
+test_invalid_request_demo=read_utils.get_sheet_as_list("../test_data/RequestDemoData.xlsx","Invalid_Data")
+print(test_invalid_request_demo)
+
